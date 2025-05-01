6f46ba58bcac1a3c4376ed8553ff64d6ff539732
Satyam20112001 <Satyam.singh152958@gmail.com>
2025-05-01 19:05:09 +0530

    Qr_Code.py

--- Qr_Code.py
+++ Qr_Code.py
@@ -72,7 +72,21 @@ if fname and lname:
         liked = st.radio("Have you liked the post?", ["Yes", "No"], index=None)
 
         if liked == "Yes":
-            st.success("वाह! काम ख़त्म, तो टाटा-बायबाय! लाइक तो मिल ही गया, अब यहां रुकना... सरासर ज़्यादती है! चलिए जनाब, हवा आने दीजिए! 😉 फिर मिलेंगे. जब कोई और 'महान' कार्य संपन्न करना हो!")
+            st.markdown(
+        """
+        <div style="text-align: center; font-size: 28px; font-weight: bold; color: green; margin-top: 30px;">
+            काम खतम अब क्या करना है रुक कर, टाटा-बायबाय! लाइक तो मिल ही गया, अब यह रुक कर क्या देख रहे हो कुछ नहीं मिलेगा! 
+            जा कर पढ़ाई लिखाई करो! IS, YS बनो, और यहा हवा आने दो! 😉 फिर मिलेंगे. जब कोई और 'महान' कार्य संपन्न करना हो!
+        </div>
+        <div style="text-align: center; margin-top: 30px;">
+            <iframe width="100%" height="500" 
+                src="https://www.youtube.com/embed/AAEXDZ77eQI?autoplay=1&mute=0&controls=1" 
+                title="Padhai Likhai Karo" frameborder="0" allowfullscreen>
+            </iframe>
+        </div>
+        """,
+        unsafe_allow_html=True
+        )
         else:
             st.markdown(
                 """
