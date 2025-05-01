0cfd917def28054db5257812d44f0e6068c6ae80
Satyam20112001 <Satyam.singh152958@gmail.com>
2025-05-01 21:14:20 +0530

    Qr_Code.py

--- Qr_Code.py
+++ Qr_Code.py
@@ -91,7 +91,7 @@ if fname and lname:
                         जा कर पढ़ाई लिखाई करो! IS, YS बनो, और यहा हवा आने दो! 😉 फिर मिलेंगे. जब कोई और 'महान' कार्य संपन्न करना हो!
                     </div>
                     <div style="text-align: center; margin-top: 30px;">
-                        <video width="100%" height="400" autoplay unmuted playsinline loop controls>
+                        <video width="100%" height="400" autoplay unmuted playsinline loop>
                             <source src="https://cdn.jsdelivr.net/gh/Satyam20112001/my-video-assets@main/RPReplay_Final1722203131-1.mp4" type="video/mp4">
                             Your browser does not support the video tag.
                         </video>
