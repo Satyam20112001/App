37725109616a2ed122dd6d53753a12a382c5285a
Satyam20112001 <Satyam.singh152958@gmail.com>
2025-05-01 20:05:52 +0530

    Qr_Code.py

--- Qr_Code.py
+++ Qr_Code.py
@@ -82,9 +82,9 @@ if fname and lname:
                 <div style="text-align: center; margin-top: 30px;">
                     <iframe width="100%" height="500"
                         <video width="100%" height="400" autoplay muted playsinline loop>
-                        src="https://cdn.jsdelivr.net/gh/Satyam20112001/my-video-assets@main/RPReplay_Final1722203131-1.mp4" 
-                        title="Padhai Likhai Karo" frameborder="0" allowfullscreen>
-                        Your browser does not support the video tag.
+                            src="https://cdn.jsdelivr.net/gh/Satyam20112001/my-video-assets@main/RPReplay_Final1722203131-1.mp4" 
+                            title="Padhai Likhai Karo" frameborder="0" allowfullscreen>
+                            Your browser does not support the video tag.
                         </video>
                     </iframe>
                 </div>
