02fb129eb942c4e2a55f2dc5ea4630bb2cd9a2bf
Satyam20112001 <Satyam.singh152958@gmail.com>
2025-05-01 19:53:04 +0530

    Qr_Code.py

--- Qr_Code.py
+++ Qr_Code.py
@@ -81,7 +81,7 @@ if fname and lname:
                 </div>
                 <div style="text-align: center; margin-top: 30px;">
                     <iframe width="100%" height="500" 
-                        src="RPReplay_Final1722203131-1.mp4" 
+                        src="https://github.com/Satyam20112001/my-video-assets/blob/main/RPReplay_Final1722203131-1.mp4" 
                         title="Padhai Likhai Karo" frameborder="0" allowfullscreen>
                     </iframe>
                 </div>
