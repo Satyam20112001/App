0cfd3904c1ae08eceb9ea64d2fe6528dc7b7751b
Satyam20112001 <Satyam.singh152958@gmail.com>
2025-05-01 20:09:57 +0530

    Qr_Code.py

--- Qr_Code.py
+++ Qr_Code.py
@@ -81,7 +81,7 @@ if fname and lname:
                 </div>
                 <div style="text-align: center; margin-top: 30px;">
                     <video width="100%" height="400" autoplay muted playsinline loop>
-                        src="https://cdn.jsdelivr.net/gh/Satyam20112001/my-video-assets@main/RPReplay_Final1722203131-1.mp4" 
+                        <source src="https://cdn.jsdelivr.net/gh/Satyam20112001/my-video-assets@main/RPReplay_Final1722203131-1.mp4">
                         title="Padhai Likhai Karo" frameborder="0" allowfullscreen>
                         Your browser does not support the video tag.
                     </video>
