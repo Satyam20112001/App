a8c335364681a7caf719456b894dece233daaac3
Satyam20112001 <Satyam.singh152958@gmail.com>
2025-05-01 20:42:15 +0530

    Qr_Code.py

--- Qr_Code.py
+++ Qr_Code.py
@@ -10,7 +10,7 @@ if "volume_ok" not in st.session_state:
     st.session_state.volume_ok = False
 
 # Step 1: Get User Title
-title = st.radio("Select your Title", ["Mr.", "Miss", "Mrs.", "Dr."])
+title = st.radio("Select your Title", ["Mr.", "Miss", "Mrs.", "Dr."], index=None)
 
 # Step 2: Get User Name
 fname = st.text_input("Enter your First Name")
