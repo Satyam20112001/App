cf62723d63bcf475554390b8264657d78e0bab8d
Satyam20112001 <Satyam.singh152958@gmail.com>
2025-05-01 20:34:46 +0530

    Qr_Code.py

--- Qr_Code.py
+++ Qr_Code.py
@@ -30,7 +30,7 @@ if fname and lname:
             """,
             unsafe_allow_html=True
         )
-        time.sleep(6.5)
+        time.sleep(7.2)
         st.session_state.audio_played = True
 
     if st.session_state.audio_played:
