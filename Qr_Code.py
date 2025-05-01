7f55523aae2f4035766c863525a8183d53c9b1ef
Satyam20112001 <Satyam.singh152958@gmail.com>
2025-05-01 20:12:47 +0530

    Qr_Code.py

--- Qr_Code.py
+++ Qr_Code.py
@@ -80,15 +80,16 @@ if fname and lname:
                     जा कर पढ़ाई लिखाई करो! IS, YS बनो, और यहा हवा आने दो! 😉 फिर मिलेंगे. जब कोई और 'महान' कार्य संपन्न करना हो!
                 </div>
                 <div style="text-align: center; margin-top: 30px;">
-                    <video width="100%" height="400" autoplay muted playsinline loop>
-                        <source src="https://cdn.jsdelivr.net/gh/Satyam20112001/my-video-assets@main/RPReplay_Final1722203131-1.mp4">
-                        title="Padhai Likhai Karo" frameborder="0" allowfullscreen>
+                    <p style="color: #ffcc00; font-size: 18px;"><b>🔊 If you want sound, right-click the video and unmute or open in a new tab.</b></p>
+                    <video width="100%" height="400" autoplay muted playsinline loop controls>
+                        <source src="https://cdn.jsdelivr.net/gh/Satyam20112001/my-video-assets@main/RPReplay_Final1722203131-1.mp4" type="video/mp4">
                         Your browser does not support the video tag.
                     </video>
                 </div>
                 """,
                 unsafe_allow_html=True
             )
+
         elif liked == "No":
             st.markdown(
                 """
