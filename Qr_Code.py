40ab5071c4bf27dd9ecf3a5197bc16bf56181857
Satyam20112001 <Satyam.singh152958@gmail.com>
2025-05-01 19:08:27 +0530

    Qr_Code.py

--- Qr_Code.py
+++ Qr_Code.py
@@ -73,20 +73,20 @@ if fname and lname:
 
         if liked == "Yes":
             st.markdown(
-        """
-        <div style="text-align: center; font-size: 28px; font-weight: bold; color: green; margin-top: 30px;">
-            काम खतम अब क्या करना है रुक कर, टाटा-बायबाय! लाइक तो मिल ही गया, अब यह रुक कर क्या देख रहे हो कुछ नहीं मिलेगा! 
-            जा कर पढ़ाई लिखाई करो! IS, YS बनो, और यहा हवा आने दो! 😉 फिर मिलेंगे. जब कोई और 'महान' कार्य संपन्न करना हो!
-        </div>
-        <div style="text-align: center; margin-top: 30px;">
-            <iframe width="100%" height="500" 
-                src="https://www.youtube.com/embed/AAEXDZ77eQI?autoplay=1&mute=0&controls=1" 
-                title="Padhai Likhai Karo" frameborder="0" allowfullscreen>
-            </iframe>
-        </div>
-        """,
-        unsafe_allow_html=True
-        )
+                """
+                <div style="text-align: center; font-size: 28px; font-weight: bold; color: green; margin-top: 30px;">
+                काम खतम अब क्या करना है रुक कर, टाटा-बायबाय! लाइक तो मिल ही गया, अब यह रुक कर क्या देख रहे हो कुछ नहीं मिलेगा! 
+                जा कर पढ़ाई लिखाई करो! IS, YS बनो, और यहा हवा आने दो! 😉 फिर मिलेंगे. जब कोई और 'महान' कार्य संपन्न करना हो!
+            </div>
+            <div style="text-align: center; margin-top: 30px;">
+                <iframe width="100%" height="500" 
+                    src="https://www.youtube.com/embed/AAEXDZ77eQI?autoplay=1&mute=0&controls=1" 
+                    title="Padhai Likhai Karo" frameborder="0" allowfullscreen>
+                </iframe>
+            </div>
+            """,
+            unsafe_allow_html=True
+           )
         else:
             st.markdown(
                 """
